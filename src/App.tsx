462c5443464e2eda9d4c3fdc4cb0bd163f5abd7c
d4t06 <huudat01234560@gmail.com>
2024-06-27 14:06:29 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Button from "./components/Button";
+
+function App() {
+   return (
+      <div className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]">
+         <Button>Click me</Button>
+      </div>
+   );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,116 @@
+import { ArrowPathIcon } from "@heroicons/react/16/solid";
+import { VariantProps, cva } from "class-variance-authority";
+import { ReactNode } from "react";
+import { Link } from "react-router-dom";
+
+const classes = {
+   active: "before:shadow-none font-[500] translate-y-[2px] text-[#cd1818]",
+   button: "inline-flex relative  items-center justify-center z-0",
+};
+
+const ButtonVariant = cva(classes.button, {
+   variants: {
+      variant: {
+         primary:
+            "before:content-[''] before:absolute before:z-[-1] before:inset-0 before:rounded-[8px] rounded-[8px]  active:translate-y-[2px] active:before:shadow-none",
+         clear: "",
+      },
+      size: {
+         clear: "",
+         primary: "px-[15px] py-[5px]",
+      },
+      colors: {
+         primary:
+            "before:border-[#a00000] text-[#fff] bg-[#cd1818] before:shadow-[0_2px_0_#a00000]",
+         second:
+            "before:border-[#ccc] text-[#333] bg-[#f6f6f6] before:shadow-[0_2px_0_#ccc]",
+         third: "before:border-[#a00000] bg-[#fff] text-[#000] before:shadow-[0_2px_0_#a00000]",
+         clear: "",
+      },
+      border: {
+         primary: "before:border-[2px]",
+         thin: "before:border-[1px]",
+         clear: "before:border-b-[2px]",
+      },
+      fontWeight: {
+         primary: "font-[500]",
+         thin: "",
+      },
+   },
+   defaultVariants: {
+      size: "primary",
+      colors: "primary",
+      variant: "primary",
+      border: "primary",
+      fontWeight: "primary",
+   },
+});
+
+interface Props extends VariantProps<typeof ButtonVariant> {
+   onClick?: () => void;
+   loading?: boolean;
+   children: ReactNode;
+   disabled?: boolean;
+   className?: string;
+   type?: HTMLButtonElement["type"];
+   href?: string;
+   active?: boolean;
+}
+export default function Button({
+   onClick,
+   disabled,
+   type = "button",
+   children,
+   loading,
+   className,
+   size,
+   variant,
+   colors,
+   href,
+   active,
+   fontWeight,
+   border,
+}: Props) {
+   const content = (
+      <>
+         {loading && <ArrowPathIcon className="w-[24px] animate-spin" />}
+         {!loading && children}
+      </>
+   );
+
+   return (
+      <>
+         {href ? (
+            <Link
+               to={href}
+               className={`${ButtonVariant({
+                  variant,
+                  size,
+                  colors,
+                  border,
+                  fontWeight,
+                  className,
+               })} ${active ? classes.active : ""}`}
+            >
+               {content}
+            </Link>
+         ) : (
+            <button
+               type={type || "button"}
+               onClick={onClick}
+               disabled={loading || disabled}
+               className={`${ButtonVariant({
+                  variant,
+                  size,
+                  colors,
+                  border,
+                  fontWeight,
+                  className,
+               })} ${active ? classes.active : ""}`}
+            >
+               {content}
+            </button>
+         )}
+      </>
+   );
+}
