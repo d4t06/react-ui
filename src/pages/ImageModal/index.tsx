f4a79c56ac8c22d45184be9e24b4284df1974715
d4t06 <huudat01234560@gmail.com>
2024-08-28 20:15:52 +0700

    prevent mouse enter event on mobile

--- src/pages/ImageModal/index.tsx
+++ src/pages/ImageModal/index.tsx
@@ -1,11 +1,13 @@
-import { MouseEventHandler, useRef, useState } from "react";
+import { MouseEventHandler, TouchEventHandler, useRef, useState } from "react";
 import ImageItem from "./ImageItem";
 import useMagnifier from "./useMagnifier";
 import { createPortal } from "react-dom";
 
 export default function ImageModal() {
    const [isOpenModal, setIsOpenModal] = useState("");
+
    const magnifierRef = useRef<HTMLDivElement>(null);
+   const isTouchMove = useRef(false);
 
    // hooks
    const { attrs } = useMagnifier({
@@ -19,13 +21,26 @@ export default function ImageModal() {
       setIsOpenModal(imageEle.src);
    };
 
+   const handleTouchMove: TouchEventHandler<HTMLImageElement> = () => {
+      isTouchMove.current = true;
+   };
+
+   const handleTouchEnd: TouchEventHandler<HTMLImageElement> = (e) => {
+      const imageEle = e.target as HTMLImageElement;
+
+      if (isTouchMove.current) isTouchMove.current = false;
+      else setIsOpenModal(imageEle.src);
+
+      e.preventDefault();
+   };
+
    return (
       <>
          <img
-            className={
-               "select-none touch-none cursor-zoom-in"
-            }
+            className={"select-none touch-none cursor-zoom-in"}
             onClick={handleImageClick}
+            onTouchEnd={handleTouchEnd}
+            onTouchMove={handleTouchMove}
             {...attrs}
             src="https://res.cloudinary.com/dalg3ayqh/image/upload/v1720193214/mobile-wars/eaywqttlk5pzmdckvy4f.jpg"
             alt=""
